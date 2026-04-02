ba6cdf969958a2e9415ad75b12a602aa0a534472
hrsalazar <hector.ssa@gmail.com>
2026-04-02 03:55:53 -0600

    Remove outdated work experience entries from siteConfig

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -138,56 +138,6 @@ module.exports = {
       duration: '1 yr',
       occupation: "Frontend Developer",
       description: "Built Liferay 6.2 portal templates and custom jQuery/AlloyUI components.",
-    },
-    {
-      institution: "Encode Club",
-      begin: {
-        month: 'Sep',
-        year: '2021'
-      },
-      duration: '3 months',
-      occupation: "Blockchain Development Bootcamp 2021",
-      description: "Become a blockchain developer and make the world a better place"
-  
-    },
-    {
-      company: "TAFE NSW",
-      begin: {
-        month: 'Mar',
-        year: '2019'
-      },
-      duration: '6 months',
-      occupation: "Front-end Developer / UX",
-      description: "Development and improvement of UI around TAFE Digital projects"
-  
-    }, {
-      company: "Metcash",
-      begin: {
-        month: 'may',
-        year: '2016'
-      },
-      duration: '5 months',
-      occupation: "Fullstack developer",
-      description: "Develop and integrate backend web services with UI using specific frameworks and libraries"
-    }, {
-      company: "AMP",
-      begin: {
-        month: 'jun',
-        year: '2015'
-      },
-      duration: '6 months',
-      occupation: "Software Developer",
-      description: "Rebuild NPS tactical solution and collaboration on back end development for PEPE"
-  
-    }, {
-      company: "Qantas Loyalty",
-      begin: {
-        month: 'May',
-        year: '2014'
-      },
-      duration: '1 yr',
-      occupation: "Frontend Developer",
-      description: "Integration and implementation of a portal instance using Liferay 6.2 platform. Creation of templates and layouts complying with web design approved by business unit. Development of customised functionality based on jQuery 1.11 and AlloyUI 2.0"
     }
     ],
   social: {
